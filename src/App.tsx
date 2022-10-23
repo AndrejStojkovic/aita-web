b4641d90c56af51318d9e57aae6acd46831d98c5
Andrej Stojkovic <andrejstojkovic03@gmail.com>
2022-10-23 22:47:38 +0200

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [title, setTitle] = useState('Title');
+  const [upvotes, setUpvotes] = useState('12.5k');
+  const [story, setStory] = useState('Story here');
+  const [flair, setFlair] = useState('Flair');
+
+  function getRandomPost() {
+    console.log('This is not the console log you are looking for.');
+  }
+
+  useEffect(() => {
+    getRandomPost();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App flex justify-center items-center'>
+      <div className='w-screen-lg max-h-screen h-screen mx-auto py-10'>
+        <div id='header'>
+          <div id='title' className='font-semibold text-2xl'>{title}</div>
+          <div id='upvotes' className='opacity-50'>
+            <span className='font-semibold'>${upvotes}</span> upvotes
+          </div>
+        </div>
+
+        <div id='main'>
+          {story}
+        </div>
+
+        <div id='buttons'>
+          <div id='thread-buttons'>
+
+          </div>
+
+          <div id='site-buttons'>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
